eb4f83782f0ff98019f1a0904241de9668813aff
parker-ford <35358924+parker-ford@users.noreply.github.com>
2024-04-09 09:41:49 -0700

    work

---
+++ src/main/java/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+    String make;
+    String model;
+    double odometer;
+
+    public Car(String make, String model){
+        this.make = make;
+        this.model = model;
+    }
+    public Car(String make){
+        this.make = make;
+    }
+
+    public String toString(){
+        return this.make + " " + this.model + " " + this.odometer;
+    }
+}
